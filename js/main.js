9114715961280960d498e54172af06f7d526efa8
David Shin <shin.david93@gmail.com>
2022-01-26 22:46:57 -0800

    Add values to table

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const getErgastAPI = async function (series, season) {
     return await data;
 }
 
+//  Parse constructor names with '_' and capitalizes the constructors' names
 function parseConstructor(constructor) {
     let parseString = constructor.charAt(0).toUpperCase() + constructor.slice(1);
     if(parseString.includes('_')) {
@@ -15,40 +16,37 @@ function parseConstructor(constructor) {
     return parseString;
 }
 
-function addToTable(tableColumn) {
+//  Input: Column value, new table row
+//  Description: Creates a new column value with the parameters and inputs into the table
+function addToTable(tableColumn, new_row) {
     let statsTable = document.getElementById('standings');
-    new_row = document.createElement('tr'); 
     new_col = document.createElement('td');
     new_col.innerText = tableColumn;
     new_row.append(new_col);
     statsTable.appendChild(new_row);
 }
 
-function formatTableInfo(standings) {
+//  Input: JSON Data File from ErgastAPI (F1 API)
+//  Description: Grabs column values from the JSON file and passes it to the addToTable function
+function formatTable(standings) {
     let address = standings.MRData.StandingsTable.StandingsLists[0].DriverStandings
     
     address.forEach(e => {
-        addToTable(e.position);
-        addToTable(e.Driver.givenName + ' ' + e.Driver.familyName);
-        addToTable(parseConstructor(e.Constructors[0].constructorId));
-        addToTable(e.points);
-        addToTable(e.wins);
+        new_row = document.createElement('tr'); 
+        addToTable(e.position, new_row);
+        addToTable(e.Driver.givenName + ' ' + e.Driver.familyName, new_row);
+        addToTable(parseConstructor(e.Constructors[0].constructorId), new_row);
+        addToTable(e.points, new_row);
+        addToTable(e.wins, new_row);
     });
-        
-
-    // address.forEach(e => console.log(
-    //     `Position: ${e.position}, Driver: ${e.Driver.givenName + ' ' + e.Driver.familyName}, Constructor: ${parseConstructor(e.Constructors[0].constructorId)}, Points: ${e.points}, Wins: ${e.wins}`
-    // ))
 }
 
-// Grab information from the form
+// When the form is submitted, form information is grabbed and used to parse the API link for the JSON data file
 const f1Form = document.getElementById('f1Table');
 f1Form.addEventListener('submit', async (e) => {
     e.preventDefault();
     let f1Series = e.target.series.value;
     let f1Season = e.target.season.value;
     let standings = await getErgastAPI(f1Series, f1Season);
-    formatTableInfo(standings);
-    
-})
-console.log(f1Form);
+    formatTable(standings);
+});
